2c02a63007ecae4c7da85db154459ac0d6fe778b
TonioGarcia07 <a.garcia.walecha@gmail.com>
2016-02-06 15:18:55 +0100

    Fichier input

--- InputData.py
+++ InputData.py
@@ -1,4 +1,9 @@
-a = 3
-print "a = ", a
+path = 'Practice Problem/Downloads'
+files = ['/right_angle.in', '/logo.in', '/learn_and_teach.in']
 
-print "antoine is the best"
+
+with open(path+files[0], 'r') as fichier:
+    contenu = map(lambda x: x.replace('\n', '').split(' '), fichier.readlines())
+    print contenu[0]
+    print contenu[1]
+    
