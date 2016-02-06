e1c2b0d24ba672cf2a8886b46c841794c27a5010
TonioGarcia07 <a.garcia.walecha@gmail.com>
2016-02-06 15:52:25 +0100

    fuck

--- Practice Problem/InputData.py
+++ Practice Problem/InputData.py
@@ -1,9 +1,6 @@
-path = 'Practice Problem/Downloads'
-files = ['/right_angle.in', '/logo.in', '/learn_and_teach.in']
-    
 def input(path, ext):
-    with open(path+files[0], 'r') as fichier:
+    with open(path+ext, 'r') as fichier:
     contenu = map(lambda x: x.replace('\n', '').split(' '), fichier.readlines())
     N, M = map(int, contenu[0])
     painting = [[contenu[1+n][0][m] for m in range(M)] for n in range(N)]
-    return painting
+    return N, M, painting
