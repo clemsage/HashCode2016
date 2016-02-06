cd4d8d32f90b9f50e8bf313031c65d753aafac42
TonioGarcia07 <a.garcia.walecha@gmail.com>
2016-02-06 15:34:46 +0100

    Fichier input opé

--- InputData.py
+++ InputData.py
@@ -4,6 +4,13 @@ files = ['/right_angle.in', '/logo.in', '/learn_and_teach.in']
 
 with open(path+files[0], 'r') as fichier:
     contenu = map(lambda x: x.replace('\n', '').split(' '), fichier.readlines())
+    N, M = map(int, contenu[0])
+    
+    print N, M
     print contenu[0]
     print contenu[1]
+    print contenu[1][0][0]
+    
+    painting = [[contenu[1+n][0][m] for m in range(M)] for n in range(N)]
+    print painting[0]
     
