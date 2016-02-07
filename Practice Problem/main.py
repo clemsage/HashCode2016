c3d284b07370a4ada0e2bacd8c10943b1a9c0bf1
TonioGarcia07 <a.garcia.walecha@gmail.com>
2016-02-07 12:44:16 +0100

    bug fixed"

--- Practice Problem/main.py
+++ Practice Problem/main.py
@@ -2,8 +2,8 @@ from InputData import *
 from tools import *
 
 #Read the data
-path = '/Downloads/'
+path = 'Downloads/'
 files = ['right_angle.in', 'logo.in', 'learn_and_teach.in']
-N, M, painting = InputData(path, files[0])
+N, M, painting = input_data(path, files[0])
 
 toile = [['.' for m in range(M)] for n in range(N)]
