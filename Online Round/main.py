0eed7b9268f133b23da886f256a865907b8794a7
TonioGarcia07 <a.garcia.walecha@gmail.com>
2016-02-11 19:44:34 +0100

    push

--- Online Round/main.py
+++ Online Round/main.py
@@ -12,7 +12,26 @@ path += files[0]
 
 with open(path, 'r') as fichier:
     contenu = fichier.readlines()
-    print contenu[0]
-    print 'ok'
+    R, C, D, T, max_load = map(int, contenu[0].split(' '))
+    P = int(contenu[1])
+    weights = map(int, list(contenu[2].split(' ')))
+    W = int(contenu[3])
+    warehouses = {}
+    id_warehouses = {}
+    for l in range(W):
+        a, b = map(int, contenu[4+2*l].split(' '))
+        store = map(int, contenu[5+2*l].split(' '))
+        warehouses[a, b] = store
+        id_warehouses[l] = [a, b]
+
+    C = int(contenu[4+2*W])
     
+    orders = {}
+    for l in range(C):
+        a, b = map(int, contenu[5+2*W+3*l].split(' '))
+        L = int(contenu[6+2*W+3*l]) # useless
+        items = map(int, contenu[7+2*W+3*l].split(' '))
+        orders[a, b] = items
+
+
 print 'Total time : %f min' % ((time.time() - start)/60.)
